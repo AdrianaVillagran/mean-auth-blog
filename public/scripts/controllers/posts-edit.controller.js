1f72100ddba956649b47c3ffda43c179328b8437
AdrianaVillagran <anvillagran1@gmail.com>
2016-04-20 12:34:20 -0700

    working on edit

--- public/scripts/controllers/posts-edit.controller.js
+++ public/scripts/controllers/posts-edit.controller.js
@@ -2,4 +2,22 @@ PostsEditController.$inject = ["$location", "$http", "$routeParams"]; // minific
 function PostsEditController ($location, $http, $routeParams) {
   var vm = this;
   vm.post = {}; // form data
+
+
+  vm.update = function() {
+    $http({
+      method: 'PUT',
+      url: '/api/posts/' + $routeParams.id,
+      data: vm.post
+    }).then(
+      function onEditSuccess(response) {
+        console.log('success editing post:', response.data);
+      },
+      function onEditError(response) {
+        console.log('error editing post:', response);
+      });
+  };
+
+
+
 }
